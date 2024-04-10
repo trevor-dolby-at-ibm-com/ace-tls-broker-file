add8822e5070cd4204c01b5f998ef3e3a1424838
Trevor Dolby <trevor.dolby@ibm.com>
2024-04-09 20:05:39 -0500

    Initial projects
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ace-tls-broker-file
 TLS connectivity using a .broker file
+
+Change the port number in the broker files to the correct port
+
+Run the `create-key-and-trust-stores.sh` script, which should create the
+key and truststores, create a broker, and configure it before starting it.
+The overrides node.conf.yaml will be updated by the script:
+```
+# This will add the TLS configuration to the existing
+# overrides node.conf.yaml, which ends with
+#
+# RestAdminListener:
+#   port: 4419
+echo "  sslCertificate: '$PWD/acekeystore.p12'" >> /var/mqsi/components/TLSBroker/overrides/node.conf.yaml
+echo "  sslPassword: 'changeit'" >> /var/mqsi/components/TLSBroker/overrides/node.conf.yaml
+```
+
+After this, running
+```
+mqsilist -n ./tls-connect-jks.broker
+```
+should connect successfully.
---
+++ create-key-and-trust-stores.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+keytool -genkey -alias acecert -keyalg RSA -keystore acekeystore.p12 -storepass changeit -storetype pkcs12 -dname "CN=aceuser, OU=ExpertLabs, O=IBM, L=Minneapolis, ST=MN, C=US"  -keypass changeit
+
+
+keytool -export -alias acecert -keystore acekeystore.p12 -storepass changeit -storetype pkcs12 -file acepublickey.cer
+
+
+keytool -import -noprompt -v -trustcacerts -alias acecert -keystore acetruststore.p12 -storepass changeit -storetype pkcs12 -file acepublickey.cer 
+keytool -import -noprompt -v -trustcacerts -alias acecert -keystore acetruststore.jks -storepass changeit -storetype jks -file acepublickey.cer 
+
+mqsicreatebroker TLSBroker
+# This will add the TLS configuration to the existing
+# overrides node.conf.yaml, which ends with
+#
+# RestAdminListener:
+#   port: 4419
+echo "  sslCertificate: '$PWD/acekeystore.p12'" >> /var/mqsi/components/TLSBroker/overrides/node.conf.yaml
+echo "  sslPassword: 'changeit'" >> /var/mqsi/components/TLSBroker/overrides/node.conf.yaml
+
+mqsistart TLSBroker
